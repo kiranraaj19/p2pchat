975c0a755c5c250e521bd549793c3ef3e3b296ac
kiranraaj19 <kiranraajsekar@gmail.com>
2023-08-02 17:25:33 +0530

    Chenged server.js to suit vercel

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ goodbye(() => swarm.destroy())
 
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
